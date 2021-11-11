12addff657c37a9311da68b6be27dc22498a38c5
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-11-11 01:13:47 +0000

    Correction on 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,13 +8,13 @@
 void display_complex_number(complex c)
 {
 	if (c.im > 0 &&  c.im != 1)
-		printf("%f + %fi\n", c.re, c.im);
+		printf("%g + %gi\n", c.re, c.im);
 	else if (c.im == 0)
-		printf("%f\n", c.re);
+		printf("%g\n", c.re);
 	else if (c.im < 0 && c.im != (-1))
-		printf("%f - %fi\n", c.re, (c.im * (-1)));
+		printf("%g - %gi\n", c.re, (c.im * (-1)));
 	else if (c.im == 1)
-		printf("%f + i\n", c.re);
+		printf("%g + i\n", c.re);
 	else
-		printf("%f - i\n", c.re);
+		printf("%g - i\n", c.re);
 }
